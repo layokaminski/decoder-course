39b9ad31c7c656889affb67ce3312a8268995131
layokaminski <layokaminski@gmail.com>
2023-08-17 18:33:46 -0300

    feat: Cria REST API do Lesson

--- src/main/java/com/ead/course/repositories/LessonRepository.java
+++ src/main/java/com/ead/course/repositories/LessonRepository.java
@@ -7,10 +7,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface LessonRepository extends JpaRepository<LessonModel, UUID> {
 
     @Query(value = "SELECT * FROM lessons WHERE module_module_id = :moduleId", nativeQuery = true)
     List<LessonModel> findAllLessonsIntoModule(@Param("moduleId") UUID moduleId);
+
+    @Query(
+            value = "SELECT * FROM lessons WHERE module_module_id = :moduleId AND lesson_id = :lessonId",
+            nativeQuery = true
+    )
+    Optional<LessonModel> findLessonIntoModule(@Param("moduleId") UUID moduleId, @Param("lessonId") UUID lessonId);
 }
